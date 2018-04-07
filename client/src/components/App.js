0bb1cf6a2333357d6a1c6df1c090b06a5e5efc24
Everistus Olumese <everistusolumese@gmail.com>
2018-04-07 11:17:42 +0100

    Added bootstrap navbar to frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Nav from "./nav";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav  />
+      <div className="container-fluid">
+        <h2> Farmers' Coop</h2>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
